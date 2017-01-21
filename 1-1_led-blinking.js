edc25b047add6624476300d52e6a36dc8a191335
Jonas Büchel <jonas.buechel@gmail.com>
2017-01-21 19:16:54 +0100

    Adapt previous exercises to eslint validity

--- 1-1_led-blinking.js
+++ 1-1_led-blinking.js
@@ -1,11 +1,11 @@
-const Tessel = require("tessel-io");
-const five = require("johnny-five");
+const Tessel = require('tessel-io');
+const five = require('johnny-five');
 
 const board = new five.Board({
   io: new Tessel()
 });
 
-board.on("ready", () => {
-  const led = new five.Led("a5");
+board.on('ready', () => {
+  const led = new five.Led('a5');
   led.blink(250);
 });
